c116bb59e90905025411abadbe3e2348e0b42584
kerem-yasir-ozyon <keremyasirozyon@gmail.com>
2024-07-28 23:20:36 +0300

    Database bağlantısı yapıldı. Entityler oluşturulmaya başlandı.

---
+++ EfCore.CodeFirst.Example/Context/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.CodeFirst.Example.Context.Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        //[Column(TypeName = "nvarchar(25)")]
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(25)]
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
+}
---
+++ EfCore.CodeFirst.Example/Context/ShopDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EfCore.CodeFirst.Example.Context.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfCore.CodeFirst.Example.Context
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=ErdemDesktop;Initial Catalog=ShopDb;Integrated Security=true;Encrypt=False");
+        }
+    }
+}
